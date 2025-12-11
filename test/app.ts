f3ae658d60a52f0d9e9e6453d274d6f757228182
vaivaeequ <vaiva@eequ.org>
2025-12-11 12:15:49 +0200

    feat: add test database

--- test/app.ts
+++ test/app.ts
@@ -1,8 +1,18 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppModule } from '../src/app.module';
+import { DataSource } from 'typeorm';
+import { typeormTestOptions } from './database.provider';
+import { TestDatabaseService } from './test-database.service';
 
 export const getMainModule = async (): Promise<TestingModule> => {
   return Test.createTestingModule({
     imports: [AppModule],
-  }).compile();
+    providers: [TestDatabaseService],
+  })
+    .overrideProvider(DataSource)
+    .useFactory({
+      factory: typeormTestOptions.useFactory,
+      inject: typeormTestOptions.inject,
+    })
+    .compile();
 };
---
+++ test/test-database.service.ts
@@ -0,0 +1,36 @@
+import { Inject, Injectable } from '@nestjs/common';
+import { DataSource, EntityMetadata } from 'typeorm';
+
+@Injectable()
+export class TestDatabaseService {
+  constructor(@Inject(DataSource) public dataSource: DataSource) {}
+
+  async closeDatabaseConnection() {
+    if (this.dataSource.isInitialized) {
+      await this.dataSource.destroy();
+    }
+  }
+
+  async cleanDatabase() {
+    const entities = await this.getEntities();
+
+    await this.cleanAllEntities(entities);
+  }
+
+  private async getEntities() {
+    return this.dataSource.entityMetadatas;
+  }
+
+  private async cleanAllEntities(entities: EntityMetadata[]) {
+    const query = [
+      'SET FOREIGN_KEY_CHECKS = 0;',
+      ...entities.map(
+        (entity) => `DELETE
+                             FROM ${entity.tableName};`,
+      ),
+      'SET FOREIGN_KEY_CHECKS = 1;',
+    ].join('');
+
+    await this.dataSource.query(query);
+  }
+}
