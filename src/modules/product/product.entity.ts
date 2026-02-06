6102c5a2c59f6c5b1deadd140689fcb0d9054d22
vaiva <vaiva.nedzviegiene@gmail.com>
2026-02-06 11:29:03 +0200

    refactor: add private fields to entities

--- src/modules/product/product.entity.ts
+++ src/modules/product/product.entity.ts
@@ -2,36 +2,60 @@ import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
 export class Product {
-  @PrimaryGeneratedColumn()
-  id: number;
+  @PrimaryGeneratedColumn({ name: 'id' })
+  private _id: number;
 
-  @Column()
-  createdDate: Date;
+  @Column({ name: 'created_date' })
+  private _createdDate: Date;
 
-  @Column()
-  updatedDate: Date;
+  @Column({ name: 'updated_date' })
+  private _updatedDate: Date;
 
-  @Column({ nullable: true })
-  title: string;
+  @Column({ name: 'title', nullable: true })
+  private _title: string;
 
-  @Column({ type: 'text', nullable: true })
-  description: string;
+  @Column({ name: 'description', type: 'text', nullable: true })
+  private _description: string;
 
-  @Column({ nullable: true })
-  number: string;
+  @Column({ name: 'number', nullable: true })
+  private _number: string;
+
+  get id(): number {
+    return this._id;
+  }
+
+  get createdDate(): Date {
+    return this._createdDate;
+  }
+
+  get updatedDate(): Date {
+    return this._updatedDate;
+  }
+
+  get title(): string {
+    return this._title;
+  }
+
+  get description(): string {
+    return this._description;
+  }
+
+  get number(): string {
+    return this._number;
+  }
 
   setTitle(title: string) {
-    this.title = title;
+    this._title = title;
     return this;
   }
 
   setDescription(description: string) {
-    this.description = description;
+    this._description = description;
     return this;
   }
 
   setNumber(number: string) {
-    this.number = number;
+    this._number = number;
     return this;
   }
 }
